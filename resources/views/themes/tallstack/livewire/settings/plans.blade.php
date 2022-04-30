f0416ab38abbdbcd2c0894d0b4c7358fcf84e77a
Eudes <36993464+SailorEudes@users.noreply.github.com>
2022-04-30 21:52:43 +0200

    Update plans.blade.php

--- resources/views/themes/tallstack/livewire/settings/plans.blade.php
+++ resources/views/themes/tallstack/livewire/settings/plans.blade.php
@@ -19,93 +19,176 @@
     @endif
     @endif
 
+    @foreach(Wave\Plan::all() as $plan)
+    @php $features = explode(',', $plan->features); @endphp
+    <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 pb-10">
 
-    <div class="flex flex-col max-w-4xl mx-auto divide-x-0 divide-gray-800 md:divide-x lg:divide-x-0 md:flex-row lg:space-x-8 pb-20">
-        @foreach(Wave\Plan::all() as $plan)
-        @php $features = explode(',', $plan->features); @endphp
-        <div class="w-full md:w-1/2 br-responsive">
-            <div class="relative w-full p-10 overflow-hidden bg-gray-900 shadow-xl lg:rounded-lg">
-                <div class="absolute top-0 left-0 w-full h-1 bg-gradient-to-br from-yellow-400 via-purple-400 to-pink-500"></div>
-                <div class="flex items-center justify-between text-gray-200">
-                    <div class="relative justif-self-end">
-                        <h2 class="text-2xl bg-clip-text  bg-gradient-to-r from-yellow-400 via-red-400 to-purple-500 text-transparent">
-                            {{ $plan->name }}
-                        </h2>
-                        <p class="text-xs font-medium uppercase">
-                            {{ $plan->description }}
-                        </p>
-                    </div>
+        <div class="pricing-box max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
+            <div class="bg-white dark:bg-gray-800 px-6 py-8 lg:flex-shrink-1 lg:p-12">
+                <h3 class="text-2xl leading-8 font-extrabold bg-clip-text bg-gradient-to-r @if($plan->price == 100) from-yellow-400 via-red-400 to-purple-500 @else from-purple-500 via-red-400 to-yellow-400 @endif text-transparent sm:text-3xl sm:leading-9 dark:text-white text-justify">
+                    {{ $plan->name }} manage
+                </h3>
+                <p class="mt-6 text-base leading-6 text-gray-500 dark:text-gray-200">
+                    {!! $plan->description !!}
+                </p>
+                <div class="mt-8">
                     <div class="flex items-center">
-                        <h3 class="text-5xl text-white">
-                            {{ $plan->price }} €
-                        </h3>
-                        <div class="flex flex-col ml-3">
-                            <p class="font-medium">
-                                per month
-                            </p>
-                            <p class="text-sm">
-                                All taxes included
-                            </p>
+                        <h4 class="flex-shrink-0 pr-4 bg-white dark:bg-gray-800 text-sm leading-5 tracking-wider font-semibold uppercase text-indigo-600">
+                            What's included
+                        </h4>
+                        <div class="flex-1 border-t-2 border-gray-200">
                         </div>
                     </div>
+                    <ul class="mt-8 lg:grid lg:grid-cols-2 lg:col-gap-8 lg:row-gap-5">
+                        @foreach($features as $feature)
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
+                                    <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                {{ $feature }}
+                            </p>
+                        </li>
+                        @endforeach
+                    </ul>
                 </div>
-                <div class="border-t border-gray-700 h-0.5 my-10 w-full"></div>
-                <ul class="w-full py-2 my-12 space-y-5 text-xl text-gray-300">
+                <div class="mt-8">
+                    <div class="flex items-center">
+                        <h4 class="flex-shrink-0 pr-4 bg-white text-sm dark:bg-gray-800 leading-5 tracking-wider font-semibold uppercase text-indigo-600">
+                            &amp; What'not
+                        </h4>
+                    </div>
+                    <ul class="mt-8 lg:grid lg:grid-cols-2 lg:col-gap-8 lg:row-gap-5">
+                        @if($plan->price == 100)
 
-                    @foreach($features as $feature)
-                    <li class="flex items-center">
-                        <div class="flex items-center justify-center w-8 h-8 mr-3 text-white bg-gray-700 rounded-full">
-                            <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-green-400">
-                                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-                            </svg>
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" class="h-6 w-6 mr-2" fill="red" viewBox="0 0 1792 1792">
+                                    <path d="M1277 1122q0-26-19-45l-181-181 181-181q19-19 19-45 0-27-19-46l-90-90q-19-19-46-19-26 0-45 19l-181 181-181-181q-19-19-45-19-27 0-46 19l-90 90q-19 19-19 46 0 26 19 45l181 181-181 181q-19 19-19 45 0 27 19 46l90 90q19 19 46 19 26 0 45-19l181-181 181 181q19 19 45 19 27 0 46-19l90-90q19-19 19-46zm387-226q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                Promotions on Foxshop throughout the year
+                            </p>
+                        </li>
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" class="h-6 w-6 mr-2" fill="red" viewBox="0 0 1792 1792">
+                                    <path d="M1277 1122q0-26-19-45l-181-181 181-181q19-19 19-45 0-27-19-46l-90-90q-19-19-46-19-26 0-45 19l-181 181-181-181q-19-19-45-19-27 0-46 19l-90 90q-19 19-19 46 0 26 19 45l181 181-181 181q-19 19-19 45 0 27 19 46l90 90q19 19 46 19 26 0 45-19l181-181 181 181q19 19 45 19 27 0 46-19l90-90q19-19 19-46zm387-226q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                Monthly maintenance
+                            </p>
+                        </li>
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" class="h-6 w-6 mr-2" fill="red" viewBox="0 0 1792 1792">
+                                    <path d="M1277 1122q0-26-19-45l-181-181 181-181q19-19 19-45 0-27-19-46l-90-90q-19-19-46-19-26 0-45 19l-181 181-181-181q-19-19-45-19-27 0-46 19l-90 90q-19 19-19 46 0 26 19 45l181 181-181 181q-19 19-19 45 0 27 19 46l90 90q19 19 46 19 26 0 45-19l181-181 181 181q19 19 45 19 27 0 46-19l90-90q19-19 19-46zm387-226q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                Up to 2 custom features
+                            </p>
+                        </li>
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" class="h-6 w-6 mr-2" fill="red" viewBox="0 0 1792 1792">
+                                    <path d="M1277 1122q0-26-19-45l-181-181 181-181q19-19 19-45 0-27-19-46l-90-90q-19-19-46-19-26 0-45 19l-181 181-181-181q-19-19-45-19-27 0-46 19l-90 90q-19 19-19 46 0 26 19 45l181 181-181 181q-19 19-19 45 0 27 19 46l90 90q19 19 46 19 26 0 45-19l181-181 181 181q19 19 45 19 27 0 46-19l90-90q19-19 19-46zm387-226q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                Unlimited access to premium components
+                            </p>
+                        </li>
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" class="h-6 w-6 mr-2" fill="red" viewBox="0 0 1792 1792">
+                                    <path d="M1277 1122q0-26-19-45l-181-181 181-181q19-19 19-45 0-27-19-46l-90-90q-19-19-46-19-26 0-45 19l-181 181-181-181q-19-19-45-19-27 0-46 19l-90 90q-19 19-19 46 0 26 19 45l181 181-181 181q-19 19-19 45 0 27 19 46l90 90q19 19 46 19 26 0 45-19l181-181 181 181q19 19 45 19 27 0 46-19l90-90q19-19 19-46zm387-226q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                Shared hosting
+                            </p>
+                        </li>
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" class="h-6 w-6 mr-2" fill="red" viewBox="0 0 1792 1792">
+                                    <path d="M1277 1122q0-26-19-45l-181-181 181-181q19-19 19-45 0-27-19-46l-90-90q-19-19-46-19-26 0-45 19l-181 181-181-181q-19-19-45-19-27 0-46 19l-90 90q-19 19-19 46 0 26 19 45l181 181-181 181q-19 19-19 45 0 27 19 46l90 90q19 19 46 19 26 0 45-19l181-181 181 181q19 19 45 19 27 0 46-19l90-90q19-19 19-46zm387-226q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                Dedicated developer
+                            </p>
+                        </li>
+                        @else
+                        <li class="flex items-start lg:col-span-1">
+                            <div class="flex-shrink-0">
+                                <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" width="6" height="6" stroke="currentColor" fill="#10b981" viewBox="0 0 1792 1792">
+                                    <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <p class="ml-3 text-sm leading-5 text-gray-700 dark:text-gray-200">
+                                Don't worry, with this offer, the "what's not" doesn't exist!
+                            </p>
+                        </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+            <div class="py-8 px-6 text-center bg-gray-50 dark:bg-gray-700 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
+                <p class="text-lg leading-6 font-bold text-gray-900 dark:text-white">
+                    Only
+                </p>
+                <div class="mt-4 flex items-center justify-center text-5xl leading-none font-extrabold bg-clip-text bg-gradient-to-r from-yellow-400 via-red-400 to-purple-500 text-transparent">
+                    <span>
+                        {{ $plan->price }}.00€
+                    </span>
+                </div>
+                <p class="mt-4 text-sm leading-5">
+                    <span class="inline-block font-medium text-gray-500 dark:text-gray-400">
+                        Every month. All taxes included.
+                    </span>
+                </p>
+                <div class="mt-6">
+                    <div class="rounded-md shadow">
+                        @subscribed($plan->slug)
+                        <div class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold transition duration-150 ease-in-out bg-gray-200 border border-transparent cursor-default text-gray-600 focus:outline-none disabled:opacity-25" disabled>
+                            You're subscribed to this plan
                         </div>
-                        <span>{{ $feature }}</span>
-                    </li>
-                    @endforeach
-
-                    @if($plan->price == 100)
-
-                    <li class="flex items-center">
-                        <div class="flex items-center justify-center w-8 h-8 mr-3 text-white bg-gray-700 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" class="w-4 h-4 text-red-400">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-                            </svg></div> <span>Shared hosting</span>
-                    </li>
-                    <li class="flex items-center">
-                        <div class="flex items-center justify-center w-8 h-8 mr-3 text-white bg-gray-700 rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" class="w-4 h-4 text-red-400">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-                            </svg>
-                        </div> <span>Unlimited Customizations</span>
-                    </li>
-                    @endif
-                </ul>
-
-                @subscribed($plan->slug)
-                <div class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold transition duration-150 ease-in-out bg-gray-200 border border-transparent cursor-default text-gray-600 focus:outline-none disabled:opacity-25" disabled>
-                    You're subscribed to this plan
+                        @notsubscribed
+                        @subscriber
+                        <div onclick="switchPlans('{{ $plan->plan_id }}', '{{ $plan->name }}')" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border border-transparent cursor-pointer focus:outline-none disabled:opacity-25">
+                            Switch Plans
+                        </div>
+                        @notsubscriber
+                        <div data-plan="{{ $plan->plan_id }}" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border border-transparent cursor-pointer checkout focus:outline-none disabled:opacity-25">
+                            Get Started
+                        </div>
+                        @endsubscriber
+                        @endsubscribed
+                    </div>
                 </div>
-                @notsubscribed
-                @subscriber
-                <div onclick="switchPlans('{{ $plan->plan_id }}', '{{ $plan->name }}')" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border border-transparent cursor-pointer focus:outline-none disabled:opacity-25">
-                    Switch Plans
-                </div>
-                @notsubscriber
-                <div data-plan="{{ $plan->plan_id }}" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border border-transparent cursor-pointer checkout focus:outline-none disabled:opacity-25">
-                    Get Started
-                </div>
-                @endsubscriber
-                @endsubscribed
-
-                <!--<a href="mailto:contact@foxpress.fr?'We Manage' Plan's&amp;body=Hi, i took a look at your 'We Manage' plan and i'm interested to know more about it, can we get in touch and discuss it?" class="relative block w-full py-4 overflow-hidden text-base font-semibold text-center text-gray-800 bg-white rounded-lg"><span>Let's talk</span></a>-->
             </div>
         </div>
-        @endforeach
+
     </div>
+    @endforeach
 
     @include('theme::partials.switch-plans-modal')
 
     @if(config('wave.paddle.env') == 'sandbox')
-    <div class="mx-auto max-w-7xl">
+    <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 pb-10">
         <div class="w-full p-10 text-white shadow-xl lg:rounded-lg bg-gray-900">
             <div class="flex items-center pb-4">
                 <svg class="mr-2 w-14 h-14 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
